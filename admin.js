83e9b514f49e69a395c108db319f86214e56aa65
Jamaldin <jamaldin@Jamaldins-MacBook-Air.local>
2025-05-15 15:54:16 +0600

    2

--- admin.js
+++ admin.js
@@ -3,6 +3,7 @@
     apiKey: "AIzaSyCjxNPm7xFPFz9QgCZzldttZFO_e-IU7ag",
     authDomain: "queue-dentist.firebaseapp.com",
     projectId: "queue-dentist",
+    databaseURL: "https://queue-dentist-default-rtdb.europe-west1.firebasedatabase.app",
     storageBucket: "queue-dentist.firebasestorage.app",
     messagingSenderId: "1052131490667",
     appId: "1:1052131490667:web:f5515886b538382e0b2a2e"
